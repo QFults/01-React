5b3fc2dd63b1092c21d6585f629b22277c6bb861
Quinton Fults <quinton.fults.work@gmail.com>
2020-12-22 14:10:29 -0800

    code

--- src/App.js
+++ src/App.js
@@ -2,17 +2,35 @@ import Navbar from './components/Navbar'
 import Card from './components/Card'
 import './App.css'
 
+let posts = [
+  {
+    title: 'My First Post',
+    body: 'This is my first post.'
+  },
+  {
+    title: 'My Second Post',
+    body: 'This is my second post.'
+  },
+  {
+    title: 'My Third Post',
+    body: 'This is my third post.'
+  },
+  {
+    title: 'My Fourth Post',
+    body: 'This is my fourth post.'
+  }
+]
+
 const App = () => {
   return (
     <div>
       <Navbar title="My First React App" />
       <div className="container">
         <div className="row">
-          <div className="col-md-6">
-            <Card />
-          </div>
-          <div className="col-md-6">
-            <Card />
+          <div className="col-md-12">
+            {
+              posts.map(post => <Card title={post.title} body={post.body} />)
+            }
           </div>
         </div>
       </div>
