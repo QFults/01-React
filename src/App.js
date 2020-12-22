a0bcdd2b10d322fab3047e3b5fa2bdc024d1c1a8
Quinton Fults <quinton.fults.work@gmail.com>
2020-12-22 11:22:52 -0800

    code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import './App.css'
+
 const App = () => {
   return (
     <div>
-      <h1>Hello World</h1>
-      <p>This paragraph is awesome</p>
+      <h1 id="myHeading">Hello World</h1>
+      <p className="myParagraph">This paragraph is awesome</p>
+      <p className="myParagraph">This paragraph is awesome</p>
+      <p className="myParagraph">This paragraph is awesome</p>
+      <button className="btn btn-primary">Click Me</button>
     </div>
   )
 }
